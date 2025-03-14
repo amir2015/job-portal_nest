d14e844bf814b6541bf1cd6bfd2384a03c66d92e
amir2015 <zahranovich2@gmail.com>
2025-03-14 15:08:35 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+async function bootstrap() {
+  dotenv.config();
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
