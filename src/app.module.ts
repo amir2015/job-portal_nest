8c61e7b02a7fec1b333d3884d179f0b0fba48171
amir2015 <zahranovich2@gmail.com>
2025-03-14 15:13:20 +0200

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,14 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma.service';
 import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
+import { CompanyModule } from './company/company.module';
+import { JobModule } from './job/job.module';
+import { ApplicationModule } from './application/application.module';
 
 @Module({
-  imports: [ConfigModule, UserModule],
+  imports: [ConfigModule, UserModule, CompanyModule, JobModule, ApplicationModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService,UserService],
 })
 export class AppModule {}
