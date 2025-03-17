011894a426a6fcd01598dfac00dcd3515568ba45
amir2015 <zahranovich2@gmail.com>
2025-03-17 14:36:10 +0200

    create-company

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { CompanyService } from './company.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { RegisterCompanyDto } from './dto/company.dto';
 
 @Controller('company')
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post('register')
+  async registerCompany(
+    @Body() registerCompanyDto: RegisterCompanyDto,
+    @Req() req: any,
+  ) {
+    const userId = req.user.id;
+    const result = await this.companyService.registerCompany(
+      registerCompanyDto,
+      userId,
+    );
+    return {
+      message: 'company registered successfully',
+      result,
+      success: true,
+    };
+  }
 }
--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class CompanyService {}
+export class CompanyService {
+  constructor(private readonly prismaService: PrismaService) {}
+  async registerCompany(registerCompanyDto: any, userId: any) {
+    const { name, description, website, location, logo } = registerCompanyDto;
+    const companyExisting = await this.prismaService.company.findUnique({
+      where: {
+        name,
+      },
+    });
+    if (companyExisting) {
+      return {
+        error: 'Company already exists',
+      };
+    }
+    const company = await this.prismaService.company.create({
+      data: {
+        name,
+        description,
+        website,
+        location,
+        logo,
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+    return company;
+  }
+}
