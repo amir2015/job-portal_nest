664f5bc629433498c377fc5905f56150f455a694
amir2015 <zahranovich2@gmail.com>
2025-03-18 21:58:46 +0200

    jobApply

--- src/application/application.controller.ts
+++ src/application/application.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { ApplicationService } from './application.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('application')
 export class ApplicationController {
   constructor(private readonly applicationService: ApplicationService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post(':id')
+  async applyJob(@Param('id') jobId: string, @Req() req: any) {
+    const applicantId = req.user.id;
+    const application = await this.applicationService.applyJob(
+      jobId,
+      applicantId,
+    );
+    return {
+      message: 'your application has been submitted',
+      application,
+      success: true,
+    };
+  }
 }
--- src/application/application.service.ts
+++ src/application/application.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class ApplicationService {}
+export class ApplicationService {
+  constructor(private prismaService: PrismaService) {}
+
+  async applyJob(jobId: string, applicantid: any) {
+    if (!jobId) {
+      throw new BadRequestException('jobId is required');
+    }
+    const existingApplication = await this.prismaService.application.findFirst({
+      where: { jobId, applicantId: applicantid },
+    });
+    if (existingApplication) {
+      throw new BadRequestException('You have already applied for this job');
+    }
+    const job = await this.prismaService.job.findUnique({
+      where: { id: jobId },
+    });
+    if (!job) {
+      throw new BadRequestException('Job not found');
+    }
+    const newApplication = await this.prismaService.application.create({
+      data: {
+        jobId,
+        applicantId: applicantid,
+      },
+    });
+    if (!newApplication) {
+      throw new BadRequestException('Application not created');
+    }
+    return newApplication;
+  }
+}
